d3613ce9bce0b2cd52c5f088f93e76a960875084
Aryanraval2030 <aryanraval1432@gmail.com>
2026-05-30 13:27:47 +0530

    updated

--- MERN-STACK/backend/routes/students/attendRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { addAttendance } from "../../controllers/studentData/attendance.js";
-import { authMiddleware } from "../../middleware/authMiddleware.js";
-import { roleMiddleware } from "../../middleware/roleMIddleware.js";
-
-export const attendRoutes = express.Router();
-attendRoutes.post("/attendance", authMiddleware, roleMiddleware, addAttendance);
--- MERN-STACK/backend/routes/students/markRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addMarks } from "../../controllers/studentData/markControllers.js";
-import { authMiddleware } from "../../middleware/authMiddleware.js";
-import { markMiddlware } from "../../middleware/markMiddlware.js";
-
-export const markRoutes = express.Router();
-
-markRoutes.post("/marks", authMiddleware, markMiddlware, addMarks);
